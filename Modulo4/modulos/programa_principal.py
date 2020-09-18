daf061b35c5eb548b3ea45d98470a2696d89d0a2
gdelgadr <gonzalo.delgado.r@uni.pe>
2020-09-17 22:12:57 -0500

    modulos

---
+++ Modulo4/modulos/programa_principal.py
@@ -0,0 +1,26 @@
+# librerias propias de python
+import os
+import sys
+
+# librerias externas
+import pandas as pd
+
+# mis librerias
+import saludar
+from mas import despedida
+
+from mas.menos import more
+
+saludar.saludar('programa_principal.py')
+
+
+saludar.adios()
+
+despedida.chau()
+
+
+despedida.otro_saludo()
+
+
+more.mas_saludos()
+
---
+++ Modulo4/modulos/saludar.py
@@ -0,0 +1,12 @@
+def saludar(desde):
+    print("Hola, te estoy saludando desde la función {} del módulo saludos".format(desde))
+
+
+def adios():
+    print('chau')
+
+
+
+# demo
+if __name__ == "__main__":
+    saludar('saludar.py')
